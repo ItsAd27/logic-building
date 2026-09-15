c3af105d85f5f831123212078ccf5e2b4006ad2e
Anuj <anujdahake05@gmail.com>
2026-09-15 15:36:26 +0530

    sum of num

---
+++ add.py
@@ -0,0 +1,37 @@
+# # l=[
+# #     1,5,9,12,53,76
+# # ]
+# # add =
+# # for i in range(len(l)):
+# #     add=add+l[i]
+
+
+# # print("Add of the listis:",add)
+
+
+# totalnumber=int(input("enter the total number:"))
+# l=[]
+
+# for i in range(totalnumber):
+#     l.append(int(input("Enter the add number")))
+
+# add = 0
+# for i in range(len(l)):
+#     add = add + l[i]
+
+# print(add)
+
+
+
+
+
+
+
+l = [1, 2,5 ,7]
+
+l[2] = 53
+l.append(99)
+l.append(45)
+for i in range(0, len(l)):
+    print(l[i])
+
